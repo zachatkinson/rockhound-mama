b0d59bffff0c5b193ccb563bf35c3e8bbfb4353f
zachatkinson <zach.atkinson85@gmail.com>
2021-04-13 01:22:49 -0400

    Removed categories page generation from gatsby-node

--- gatsby-node.js
+++ gatsby-node.js
@@ -58,30 +58,7 @@ exports.createPages = ({graphql, actions}) => {
                 },
             })
         })
-        result.data.collections.edges.forEach(({node}) => {
-            const products = node.products.map(product => product.handle)
-            const numProducts = products.length
-            const productsPerPage = 12
-            const numPages = Math.ceil(products.length / productsPerPage)
-            Array.from({length: numPages}).forEach((_, i) => {
-                createPage({
-                    path: i === 0 ? `/collection/${node.handle}/` : `/collection/${node.handle}/${i + 1}`,
-                    component: path.resolve(`./src/components/shopify/templates/collectionSingle/collectionSingle.js`),
-                    context: {
-                        // Data passed to context is available
-                        // in page queries as GraphQL variables.
-                        handle: node.handle,
-                        products: products,
-                        limit: productsPerPage,
-                        skip: i * productsPerPage,
-                        numPages,
-                        numProducts,
-                        currentPage: i + 1,
-                    },
-                })
-            })
 
-        })
     })
 
 }
