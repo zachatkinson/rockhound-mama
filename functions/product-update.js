e4549134045b883505785aadbd173f650bbbe826
zachatkinson <zach.atkinson85@gmail.com>
2021-04-13 13:34:23 -0400

    product update function finalization

--- functions/product-update.js
+++ functions/product-update.js
@@ -1,7 +1,7 @@
 
 const faunadb = require('faunadb')
 const verifyWebhookIntegrity = require('shopify-verify-webhook')
-
+const axios = require('axios')
 
 const q = faundadb.query
 const client = new faunadb.Client({
@@ -16,10 +16,36 @@ exports.handler = function(event, context, callback) {
     )
 
     if(isValid){
-        callback(null, {
-            statusCode: 200,
-            body: "Hello World!",
+        const body = JSON.parse(event.body)
+        const {id} = body
+        delete body.updated_at
+        body.variants.forEach(variant => {
+            delete variant.updated_at
+            delete variant.inventory_quantity
+            delete variant.old_inventory_quantity
         })
+        const bodyString = JSON.stringify(body)
+
+        client.query(q.Get(q.Match(q.Index('product_by_id'), id))).then(result => {
+            if(result.data.product !== bodyString){
+                client.query(q.Update(result.ref, {
+                    data: {product: bodyString}
+                })).then(() => {
+                    axios.post(process.env.NETLIFY_BUILD_URL)
+                }).catch(e => {
+                    console.log("error updating product: ", e)
+                })
+            }
+        }).catch(() => {
+            client.query(q.Create(q.Collection('products'), {
+                data: {id, product: bodyString}
+            })).then(() => {
+                axios.post(process.env.NETLIFY_BUILD_URL)
+            }).catch((e) => {
+                console.log('Error adding to DB: ', e)
+            })
+        })
+
     }else{
         callback(null, {
             statusCode: 403,
