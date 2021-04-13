5a5114608d3c0ddb20263b68cf1fbf9c6e614a8e
zachatkinson <zach.atkinson85@gmail.com>
2021-04-13 12:50:02 -0400

    advanced webhooks added for faunadb

--- functions/product-update.js
+++ functions/product-update.js
@@ -1,6 +1,31 @@
+
+const faunadb = require('faunadb')
+const verifyWebhookIntegrity = require('shopify-verify-webook')
+
+
+const q = faundadb.query
+const client = new faunadb.Client({
+    secret: process.env.FAUNDADB_SECRET
+})
+
 exports.handler = function(event, context, callback) {
-    callback(null, {
-        statusCode: 200,
-        body: "Hello World!",
-    })
+    const isValid = verifyWebhookIntegrity(
+        process.env.SHOPIFY_WEBHOOK_KEY,
+        event.headers['x-shopify-hmac-sha256'],
+        event.body
+    )
+
+    if(isValid){
+        callback(null, {
+            statusCode: 200,
+            body: "Hello World!",
+        })
+    }else{
+        callback(null, {
+            statusCode: 403,
+            body: "Error",
+        })
+    }
+
+
 }
