74cbfcb1013a0962f77ab0f2d833fc162ac02e5d
zachatkinson <zach.atkinson85@gmail.com>
2021-04-14 23:58:44 -0400

    Changed button attribute from name to aria-label

--- src/components/search/search.js
+++ src/components/search/search.js
@@ -19,7 +19,7 @@ const Search = () => {
     return (
     <SearchForm onSubmit={handleSubmit}>
         <input value={searchTerm} className={`search`} placeholder={`Search`} onChange={(e) => setSearchTerm(e.currentTarget.value)}  />
-        <button name={`Search Button`}>
+        <button aria-label={`Search Button`}>
             <FaSearch />
         </button>
     </SearchForm>
