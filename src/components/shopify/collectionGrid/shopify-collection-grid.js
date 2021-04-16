6821119d4f118c61c4c0650405fad415f4527f79
zachatkinson <zach.atkinson85@gmail.com>
2021-04-15 22:22:14 -0400

    Updated collectionGrid SCSS to styled-components

--- src/components/shopify/collectionGrid/shopify-collection-grid.js
+++ src/components/shopify/collectionGrid/shopify-collection-grid.js
@@ -14,6 +14,7 @@ const CollectionGrid = ({collections}) => {
             <CollectionCard
                 collection={bundleCollection}
                 key={bundleCollection.shopifyId}
+                className={`lead-img`}
             />
             }
             <RemainingCollections>
